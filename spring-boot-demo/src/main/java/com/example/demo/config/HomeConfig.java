246c546a6f33d6247413b2707294a29986f7f1b5
vizee <vizee@live.cn>
2023-07-02 18:45:20 +0800

    feat: configuratio

---
+++ spring-boot-demo/src/main/java/com/example/demo/config/HomeConfig.java
@@ -0,0 +1,12 @@
+package com.example.demo.config;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Data
+@Configuration
+@ConfigurationProperties(prefix = "app.home")
+public class HomeConfig {
+    String greeting;
+}
--- spring-boot-demo/src/main/java/com/example/demo/controller/HomeController.java
+++ spring-boot-demo/src/main/java/com/example/demo/controller/HomeController.java
@@ -1,5 +1,7 @@
 package com.example.demo.controller;
 
+import com.example.demo.config.HomeConfig;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,8 +10,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/home")
 public class HomeController {
 
+    @Autowired
+    HomeConfig config;
+
     @GetMapping("/hello")
     public String hello() {
-        return "world";
+        return config.getGreeting() + "world";
     }
 }
